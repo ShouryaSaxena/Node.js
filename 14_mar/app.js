9402832f2f0a0a8c55e6be2cf0b19917a17e40f3
ShouryaSaxena <shouryasaxenacse@gmail.com>
2023-03-14 15:23:10 +0530

    Commit Changes

--- 14_mar/app.js
+++ 14_mar/app.js
@@ -1,34 +1,35 @@
-
+// server file 
+// config file is for configuring the myswl server 
+// setting  up  the web server 
 const express = require("express");
-const router = require("./routes/reserveRoutes.js")
-const cors = require('cors');
-const path = require('path')
-
+const cors = require("cors");       // cors stand for cross origin resource sharing 
+const router = require("./route/resultRoutes.js")
 const db = require("./model");
-const app = express();
-
+const app =express();
+const path =require("path");
 db.sequelize.sync();
 
 let corsOptions = {
-    origin:"http://localhost:4000"                   // setting up origin to 2000
+    origin:"http://localhost:3000"                   // setting up origin to 5000
 };
+
+app.use(express.static(path.join((__dirname,"public"))));  // run the html static file on browser
+
+
 app.use(cors(corsOptions));
 
-app.use("/",router);
-
-app.use(express.static(path.join((__dirname,"public"))));
+app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 
-app.use(express.json());
-
 app.get("/",(req,res)=>{
-    res.status(200).json({message:"This is the Main Page"});
+    res.status(200).json({message:"welcome to the page "});
 })
 
-//---------------------------------Establishing the msSQL connection-----------------------------
-const PORT = process.env.PORT || 4040
-app.listen(PORT, () => { console.log(`Result System listning at port ${PORT}`) });
+app.use("/",router);
 
-
-
+// setting up port and running the app 
+const PORT = process.env.PORT || 2040
+app.listen(PORT,()=>{
+    console.log(`Running at port:${PORT}`);         // listening on the port 5500 for any requests 
+});
--- 14_mar/model/index.js
+++ 14_mar/model/index.js
@@ -1,27 +1,27 @@
 // making the  sequelize connections 
-const dbconfig = require("../config.js");  // importing the dbconfig module
+const dbconfig = require("../db.config.js");  // importing the dbconfig module
 const Sequelize = require("sequelize");              // importing the sequelize module 
 
-const sequelize = new Sequelize(dbconfig.DB, dbconfig.USER, dbconfig.PASSWORD, {
+const sequelize = new Sequelize(dbconfig.DB,dbconfig.USER,dbconfig.PASSWORD,{
 
-    host: dbconfig.HOST,
-    port: dbconfig.PORT,
-    dialect: dbconfig.dialect,
+    host:dbconfig.HOST,
+    port:dbconfig.PORT,
+    dialect:dbconfig.dialect,
 
-    pool: {
+    pool:{
 
-        max: dbconfig.pool.max,
-        min: dbconfig.pool.min,
-        acquire: dbconfig.pool.acquire,
-        idle: dbconfig.pool.idle,
+        max:dbconfig.pool.max,
+        min:dbconfig.pool.min,
+        acquire:dbconfig.pool.acquire,
+        idle:dbconfig.pool.idle,
     },
 });
 
-const student = {};
+const db = {};
 
-student.Sequelize = Sequelize;
-student.sequelize = sequelize;
+db.Sequelize = Sequelize;
+db.sequelize =sequelize;
 
-student.reserve = require("./reservation.js")(sequelize, Sequelize);
+db.student = require("./student.js")(sequelize,Sequelize); 
 
-module.exports = student;
+module.exports = db;
