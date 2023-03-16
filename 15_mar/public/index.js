108e57ee79c0dce29c394102dabf6cc8dd5b380c
ShouryaSaxena <shouryasaxenacse@gmail.com>
2023-03-16 12:34:14 +0530

    Task Completed

--- 15_mar/public/index.js
+++ 15_mar/public/index.js
@@ -9,7 +9,7 @@ const showImage = async() => {
     console.log(results);
 
     for(let i=0;i<results.length;i++){
-        profileImg.innerHTML += `<img src="${results[i].img.data}" alt="${results[i].studentName}" style="width:100px; height:100px">`;
+        profileImg.innerHTML += `<img src="${results[i].img.data}" alt="${results[i].studentName}" style="width:200px; height:200px">`;
     }
 
 } 
