431055ee63726a03ba50bf4a41fbf7bc95d13a4f
ShouryaSaxena <shouryasaxenacse@gmail.com>
2023-03-17 18:47:38 +0530

    Task Commited

--- 15_mar/controllers/resultDataControl.js
+++ 15_mar/controllers/resultDataControl.js
@@ -66,7 +66,7 @@ const findByID = async(req, res) => {
 const updateResult = async (req, res) => {
     console.log(req.body);
     let updated;
-    updated = await student.findOneAndUpdate({ rollno: req.body.rollno }, {
+    updated = await student.findOneAndUpdate({ id: data.id }, {
         studentName: req.body.studentName,
         rollno: req.body.rollno,
         subjects: req.body.subjects,
